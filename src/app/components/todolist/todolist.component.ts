d96421d84d8aa3c55658bc6305ec6c52cdcdcaea
Max <maxim123456789101112@gmail.com>
2019-02-01 23:07:01 +0300

    todoList-govnokod-done

--- src/app/components/todolist/todolist.component.ts
+++ src/app/components/todolist/todolist.component.ts
@@ -6,9 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todolist.component.css']
 })
 export class TodolistComponent implements OnInit {
-  tasks: object[]
-  ngOnInit() {
-    
-  }
+  tasks: object[] = [];
 
+  ngOnInit() {
+
+  }
+  
+  addTask(task){
+    let newTask = {
+      label: task,
+      done: false,
+    };
+    this.tasks.push(newTask)
+  }
+  deleteElement(index: number){
+    this.tasks.splice(index, 1);
+  }
+  line__through(index: number){
+    this.tasks[index].done = !this.tasks[index].done;
+    console.log(this.tasks[index].done)
+  }
 }
