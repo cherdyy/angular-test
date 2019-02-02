50cabe4345c06475f9797efe774a1986ef22f1a1
Max <maxim123456789101112@gmail.com>
2019-02-02 15:46:31 +0300

    some fix todo

--- src/app/components/todolist/todolist.component.ts
+++ src/app/components/todolist/todolist.component.ts
@@ -8,23 +8,27 @@ import { Component, OnInit } from '@angular/core';
 export class TodolistComponent implements OnInit {
 
   tasks: { label: string, done: boolean }[] = [];
-
-
+  task__count: number = 0;
   ngOnInit() {
 
   }
-  
+
   addTask(task){
     let newTask = {
       label: task,
       done: false,
     };
-    this.tasks.push(newTask)
+    if (newTask.label !== '') {
+      this.tasks.push(newTask);
+      this.task__count += 1;
+    }
   }
   deleteElement(index: number){
     this.tasks.splice(index, 1);
+    this.task__count -= 1;
   }
   line__through(index: number){
     this.tasks[index].done = !this.tasks[index].done;
   }
+
 }
