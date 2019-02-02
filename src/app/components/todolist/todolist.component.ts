ab001f93760b58b7555540099efb2a773c51d562
VladislavZubko <vladyslav.zubko@nure.ua>
2019-02-02 15:27:18 +0200

    Code refuctor

--- src/app/components/todolist/todolist.component.ts
+++ src/app/components/todolist/todolist.component.ts
@@ -1,33 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-todolist',
   templateUrl: './todolist.component.html',
   styleUrls: ['./todolist.component.css']
 })
-export class TodolistComponent implements OnInit {
+export class TodolistComponent {
 
+  /**
+   * Array of task for todo list
+   * @type {{ label: string, done: boolean }[]}
+   */
   tasks: { label: string, done: boolean }[] = [];
-  task__count: number = 0;
-  ngOnInit() {
 
-  }
+  /**
+   * @ignore
+   */
+  constructor() {}
 
-  addTask(task){
+  /**
+   * Add new task
+   * @param {string} task
+   */
+  addTask(task: string) {
     let newTask = {
       label: task,
       done: false,
     };
     if (newTask.label !== '') {
       this.tasks.push(newTask);
-      this.task__count += 1;
     }
   }
-  deleteElement(index: number){
+
+  /**
+   * Delete selected task
+   * @param {number} index
+   */
+  deleteTask(index: number) {
     this.tasks.splice(index, 1);
-    this.task__count -= 1;
   }
-  line__through(index: number){
+
+  /**
+   * Cross done task
+   * @param {number} index
+   */
+  lineThrough(index: number) {
     this.tasks[index].done = !this.tasks[index].done;
   }
 
